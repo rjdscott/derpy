dd98035543aa3f5e890e2167b2a0465cadb6ad58
ParkerGuan <parker200500303@gmail.com>
2018-08-29 11:10:42 +1000

    allow ytm to take current bond price; added explanatory notes

--- derpy/bonds/bond.py
+++ derpy/bonds/bond.py
@@ -16,26 +16,38 @@ def bond_price(rate, maturity, coupon_rate, coupon_freq, face_value, due=0):
 
     return npv
 
-def bond_yield_to_maturity(maturity, coupon_rate, face_value, coupon_freq):
-    """
-    :param maturity: number of years to maturity
-    :param coupon: coupon rate of the bond
-    :param face_value: faceValue of the bond
-    :param freq: frequency of the compounding
-    :return: the irr (YTM) of the bond
+
+def bond_yield_to_maturity(price, maturity, coupon_rate, face_value, coupon_freq):
     """
 
-    cash_flow = [-1 * face_value]
+    :param price: current price of the bond
+    :param maturity: number of years to maturity
+    :param coupon_rate: coupon rate of the bond
+    :param face_value: faceValue of the bond
+    :param coupon_freq: frequency of the compounding
+    :return: the irr (YTM) of the bond
+
+    Notes: The yield to maturity (irr) calculation uses the numpy.irr library,
+           which adopts a method that finds the root of a polynomial by solving
+           the eigenvalue of the companion matrix. For a detailed explanation,
+           please see http://web.mit.edu/18.06/www/Spring17/Eigenvalue-Polynomials.pdf
+
+           There are other faster, and generic ways to solve for the root of polynomial.
+           i.e. the Newton-Raphson method. For an example of YTM calculation using this
+           method please see http://web.mit.edu/18.06/www/Spring17/Eigenvalue-Polynomials.pdf
+    """
+
+    cash_flow = [-1 * price]
     cash_flow.extend([face_value * coupon_rate / coupon_freq] * (maturity * coupon_freq - 2))
     cash_flow.append(face_value * (1 + coupon_rate / coupon_freq))
 
     ytm = np.irr(cash_flow)
 
-    return round(ytm)
+    return ytm
 
 
 class Bond(object):
-    
+
     def __init__(self, face_value=None, cpn_rate=None, cpn_freq=None, maturity=None):
         self.face_value = face_value
         self.coupon_rate = cpn_rate
@@ -52,17 +64,17 @@ class Bond(object):
                                                                                     self.coupon_freq,
                                                                                     self.maturity)
 
-    def get_yield_to_maturity(self): 
-        
+    def get_yield_to_maturity(self):
+
         if self.maturity is None:
             raise ValueError("Bond maturity is None, please set variable before recalculating...")
-        
+
         elif self.coupon_freq is None:
             raise ValueError("Bond coupon_freq is None, please set variable before recalculating...")
-        
+
         elif self.coupon_rate is None:
             raise ValueError("Bond coupon_rate is None, please set variable before recalculating...")
-        
+
         elif self.face_value is None:
             raise ValueError("Bond face_value is None, please set variable before recalculating...")
 
@@ -71,7 +83,7 @@ class Bond(object):
                                                             maturity=self.maturity,
                                                             coupon_rate=self.coupon_rate,
                                                             coupon_freq=self.coupon_freq)
-        
+
         except ValueError:
             raise("Cannot price bond, please check inputs...")
 
--- derpy/bonds/bond_calcs.py
+++ derpy/bonds/bond_calcs.py
@@ -23,20 +23,30 @@ def bond_price(rate, maturity, coupon, face_value, freq, due=0):
     return npv
 
 
-def ytm_calculation(maturity, coupon, face_value, freq):
+def ytm_calculation(price, maturity, coupon, face_value, freq):
 
     """
+    :param price: current price of the bond
     :param maturity: number of years to maturity
     :param coupon: coupon rate of the bond
     :param face_value: faceValue of the bond
     :param freq: frequency of the compounding
     :return: the irr (YTM) of the bond
+
+    Notes: The yield to maturity (irr) calculation uses the numpy.irr library,
+           which adopts a method that finds the root of a polynomial by solving
+           the eigenvalue of the companion matrix. For a detailed explanation,
+           please see http://web.mit.edu/18.06/www/Spring17/Eigenvalue-Polynomials.pdf
+
+           There are other faster, and generic ways to solve for the root of polynomial.
+           i.e. the Newton-Raphson method. For an example of YTM calculation using this
+           method please see http://web.mit.edu/18.06/www/Spring17/Eigenvalue-Polynomials.pdf
     """
 
-    cash_flow = [-1*face_value]
+    cash_flow = [-1*price]
     cash_flow.extend([face_value * coupon / freq] * (maturity*freq - 2))
     cash_flow.append(face_value*(1 + coupon/freq))
 
     ytm = np.irr(cash_flow)
 
-    return round(ytm)
+    return ytm
