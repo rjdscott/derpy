d94494ae2ffb6f3fd05291b27c3258f589db1347
rjdscott <rob@rjdscott.com>
2018-09-20 13:15:47 +1000

    removed old file from testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 """Unit test package for derpy."""
-from . import test_derpy, test_option, test_portfolio
+from . import test_option, test_portfolio
 from derpy.option import *
 
