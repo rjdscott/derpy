3ebf6d8ebecd9bb8eb6ac13259a963cceb2f8c82
rjdscott <rob@rjdscott.com>
2018-09-21 16:31:40 +1000

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rjdscott/derpy',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=True,
 )
